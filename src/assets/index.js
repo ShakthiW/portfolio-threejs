cdbff8b639426d000f1fe8c614a871b1ba4066b2
Shakthi Warnakulasuriya <shakthiraveen2003@gmail.com>
2023-09-12 13:31:14 +0530

    Initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,58 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import html from "./tech/html.png";
+import python from "./tech/python.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import nextjs from "./tech/nextjs.png";
+import express from "./tech/express.png";
+
+import bandaranayake from "./company/bandaranayake.png";
+import iit from "./company/iit.png";
+import mozilla from "./company/mozilla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  bandaranayake,
+  iit,
+  mozilla,
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  express,
+  nextjs,
+  css,
+  figma,
+  html,
+  javascript,
+  mongodb,
+  python,
+  nodejs,
+  reactjs,
+  tailwind,
+  threejs,
+  carrent,
+  jobit,
+  tripguide,
+};
